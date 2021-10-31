151d3313efb6ff08ccbb56b7f0c3a551258e4e15
Bruno Conti <brunogabriel.fortunato@gmail.com>
2021-10-31 13:47:38 -0300

    seed for states, cities and universities

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,60 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+# puts "Cleaning up database..."
+# Model.destroy_all
+# puts "Database cleaned"
+
+# ------------------------------------
+
+# puts "Start creating states"
+
+# uri = URI("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+# response = Net::HTTP.get(uri)
+# hash_array = JSON.parse(response)
+
+# hash_array.each do |hash|
+#   puts "creating #{hash['nome']}"
+#   State.create!(
+#     name: hash['nome'],
+#     initials: hash['sigla']
+#   )
+# end
+# puts "States created"
+
+# ------------------------------------
+
+# puts "Start creating cities"
+
+# uri = URI("https://sisusimulator.com.br/api/searchcity")
+# response = Net::HTTP.get(uri)
+# hash_array = JSON.parse(response)
+
+# hash_array.each do |hash|
+#   puts "creating #{hash['nome']}"
+#   Citie.create!(
+#     state: State.find_by(initials: hash["uf"]),
+#     name: hash['nome']
+#   )
+# end
+# puts "Cities created"
+
+# ------------------------------------
+
+# puts "Start creating universities"
+
+# uri = URI("https://sisusimulator.com.br/api/searchuniversidades/")
+# response = Net::HTTP.get(uri)
+# hash_array = JSON.parse(response)
+
+# hash_array.each do |hash|
+#   puts "creating #{hash['nome'].downcase.split('-')[0].split.map { |word| word.length >= 3 ? word.capitalize : word}.join(' ')}"
+#   Institution.create!(
+#     name: hash['nome'].downcase.split('-')[0].split.map { |word| word.length >= 3 ? word.capitalize : word}.join(' '),
+#     initials: hash['slug'].upcase
+#   )
+# end
+
+# puts "Universities created"
+
+# ------------------------------------
