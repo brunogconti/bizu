a7e9eff64ff0a59d1958753febc505d790504693
Bruno Conti <brunogabriel.fortunato@gmail.com>
2021-10-31 11:29:26 -0300

    navbar simple configuration

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,4 +1,6 @@
 class CoursesController < ApplicationController
+  skip_before_action :authenticate_user!, only: %I[index show]
+
   def index
   end
 
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,3 @@
 class Course < ApplicationRecord
-  belongs_to :campus
+  belongs_to :buildings_complex
 end
