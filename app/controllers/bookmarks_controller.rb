91c50b33ddcca1b9f97d31b13c4ccefaa606468d
Bruno Conti <brunogabriel.fortunato@gmail.com>
2021-11-09 10:58:42 -0300

    botão do fav e algumas alterações nos controllers

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,24 +1,29 @@
 class BookmarksController < ApplicationController
-  skip_before_action :authenticate_user!, only: %I[index create destroy]
+  skip_before_action :authenticate_user!, only: %I[index]
 
   def index
     @bookmarks = Bookmark.all.where(user: current_user)
   end
 
   def create
-    @course = Course.new(params[:course_id])
+    raise
+    @course = Course.find(params[:bookmark][:course_id])
     @bookmark = Bookmark.new(bookmark_params)
     @bookmark.user = current_user
     @bookmark.course = @course
-    if @bookmark.save
-      redirect_to bookmarks_path
+    if @bookmark.save!
+      redirect_to course_path(@course)
     else
       render 'courses/show'
     end
   end
 
   def destroy
-    @bookmark = Bookmark.find(params[:id])
+    @course = Course.find(params[:bookmark][:course_id])
+    @bookmark = Bookmark.find_by(course: @course, user: current_user)
+    # @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to course_path(@course), notice: 'bookmark was successfully destroyed.'
   end
 
   private
--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -3,10 +3,31 @@ class CoursesController < ApplicationController
 
   def index
     @courses = Course.all
+    # if params[:keyword].present?
+    #   sql_query = " \
+    #     rooms.title @@ :query \
+    #     OR rooms.description @@ :query \
+    #     OR rooms.address @@ :query \
+    #   "
+    #   @courses = Course.where(sql_query, query: "%#{params[:keyword]}%")
+    # end
+
+    # if params[:city_size].present?
+    #   @courses = @courses.where()
+    # end
+    # @markers = @rooms.geocoded.map do |room|
+    #   {
+    #     lat: room.latitude,
+    #     lng: room.longitude,
+    #     info_window: render_to_string(partial: "info_window", locals: { room: room }),
+    #     image_url: helpers.asset_url('https://cdn4.iconfinder.com/data/icons/map-pins-2/256/15-512.png')
+    #   }
+    # end
   end
 
   def show
     @course = Course.find(params[:id])
+    @favorite = Bookmark.find_by(course: @course, user: current_user)
     @bookmark = Bookmark.new
     @review = Review.new
   end
