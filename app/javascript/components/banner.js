ecded4df98b44af34ca91269e05fb07e8a8172e9
Bruno Conti <brunogabriel.fortunato@gmail.com>
2021-11-10 15:03:18 -0300

    js correction

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -9,52 +9,54 @@ import Typed from 'typed.js';
 //     console.log(item.innerText);
 //   });
 
-const list = [ 'Ciências Biológicas: Biofísica ',
-'Ciências Biológicas',
-'Enfermagem e Obstetrícia',
-'Farmácia',
-'Nutrição',
-'Química',
-'Abi - Letras - Português - Arabe',
-'Abi - Letras - Português - Frances',
-'Abi - Letras - Português - Hebraico',
-'Abi - Letras - Português - Italiano',
-'Abi - Letras - Português - Latim',
-'Abi - Letras - Português - Russo',
-'Abi - Literaturas de Língua Portuguesa',
-'Ciência da Computação',
-'Ciências Biológicas',
-'Ciências Biológicas',
-'Ciências Biológicas: Microbiologia e Imunologia',
-'Ciências Matemáticas e da Terra',
-'Dança',
-'Educação Artística - Desenho',
-'Educação Artística Com Habilitação em Artes Plásticas',
-'Educação Física',
-'Enfermagem e Obstetrícia',
-'Engenharia(ciclo Básico)',
-'Engenharia Civil',
-'Engenharia de Bioprocessos',
-'Engenharia de Controle e Automação',
-'Engenharia de Petróleo',
-'Engenharia de Produção',
-'Engenharia Eletrônica e de Computação',
-'Engenharia Metalúrgica',
-'Engenharia Nuclear',
-'Engenharia Química',
-'Estatística',
-'Farmácia',
-'Física',
-'Medicina',
-'Fisioterapia']
 
 
 const loadDynamicBannerText = () => {
-  new Typed('#banner-typed-text', {
-    strings: list,
-    typeSpeed: 80,
-    loop: true
-  });
+  const list = [ 'Ciências Biológicas: Biofísica ',
+  'Ciências Biológicas',
+  'Enfermagem e Obstetrícia',
+  'Farmácia',
+  'Nutrição',
+  'Química',
+  'Abi - Letras - Português - Arabe',
+  'Abi - Letras - Português - Frances',
+  'Abi - Letras - Português - Hebraico',
+  'Abi - Letras - Português - Italiano',
+  'Abi - Letras - Português - Latim',
+  'Abi - Letras - Português - Russo',
+  'Abi - Literaturas de Língua Portuguesa',
+  'Ciência da Computação',
+  'Ciências Biológicas',
+  'Ciências Biológicas',
+  'Ciências Biológicas: Microbiologia e Imunologia',
+  'Ciências Matemáticas e da Terra',
+  'Dança',
+  'Educação Artística - Desenho',
+  'Educação Artística Com Habilitação em Artes Plásticas',
+  'Educação Física',
+  'Enfermagem e Obstetrícia',
+  'Engenharia(ciclo Básico)',
+  'Engenharia Civil',
+  'Engenharia de Bioprocessos',
+  'Engenharia de Controle e Automação',
+  'Engenharia de Petróleo',
+  'Engenharia de Produção',
+  'Engenharia Eletrônica e de Computação',
+  'Engenharia Metalúrgica',
+  'Engenharia Nuclear',
+  'Engenharia Química',
+  'Estatística',
+  'Farmácia',
+  'Física',
+  'Medicina',
+  'Fisioterapia']
+  if (document.getElementById("banner-typed-text")) {
+    new Typed('#banner-typed-text', {
+      strings: list,
+      typeSpeed: 80,
+      loop: true
+    });
+  }
 }
 
 export { loadDynamicBannerText };
